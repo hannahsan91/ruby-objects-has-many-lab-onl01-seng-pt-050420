766fe41b2872483a2ccc42f777fefc139c9a3551
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-29 02:18:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,26 @@
 class Author
 
-  attr_accessor :name, :posts
+  attr_accessor :name
 
-  @@post_count = 0
+  @@post_count = 1
 
   def initialize(name)
     @name = name
-    @posts = []
   end
 
   def add_post(post)
-    @posts << post
-    post.author = self
+    post.author=self
     @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    add_post(post)
+    post.author=self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
   def self.post_count
