c0bddaa9f648de3423b2d65bd2bb43ab9aa16802
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-29 01:56:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,16 @@
 class Post
-  attr_accessor :name
+
+  attr_accessor :title, :Author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
 end
