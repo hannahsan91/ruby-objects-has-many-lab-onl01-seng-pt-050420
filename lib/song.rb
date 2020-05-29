659491ef2b7578943755bcf9478562d45ffd72c6
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-29 02:18:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,29 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist
   end
 
   def artist_name
     if @artist == nil
       return nil
-    else
-      @artist.name
     end
+    @artist.name
+  end
+
+  def self.all
+    @@all
   end
 end
