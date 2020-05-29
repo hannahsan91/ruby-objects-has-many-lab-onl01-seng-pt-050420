ce2c5b4a47879b129fd59804e7696ce11e6f274a
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-29 02:15:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,19 +3,13 @@ class Artist
 
   attr_accessor :name, :songs
 
-  @@all = []
+  @@song_count = []
 
   def initialize(name)
     @name = name
-    @@all << self
-  end
-  def self.all
-    @@all
   end
 
   def add_song(song)
-    binding.pry
-    @songs << song
     song.artist = self
     @@song_count += 1
   end
